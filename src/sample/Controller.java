47bcc28da4363e64e1c9f5749df4b9697a1c5d55
xiexyong <xiexyong@users.noreply.github.com>
2020-10-11 00:58:50 +0700

    Day 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javax.swing.text.html.ImageView;
+
 public class Controller {
+    @FXML
+    public ImageView search;
 }
