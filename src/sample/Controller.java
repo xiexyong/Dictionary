7d8462135d152cf7eeafa9c30e504942a5e62344
xiexyong <xiexyong@users.noreply.github.com>
2020-10-20 01:07:38 +0700

    day 3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,12 @@ package sample;
 
 import Commandline12.Dictionary;
 import Commandline12.Word;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-import java.awt.event.KeyEvent;
 import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.HashSet;
@@ -15,8 +15,10 @@ import java.util.ResourceBundle;
 import java.util.Set;
 
 import static Commandline12.Dictionary.listWord;
-import org.controlsfx.control.textfield.AutoCompletionBinding;
-import org.controlsfx.control.textfield.TextFields;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class Controller implements Initializable {
     @FXML
@@ -27,10 +29,7 @@ public class Controller implements Initializable {
     private TextArea textArea,textArea1;
     @FXML
     private ListView listView;
-    //private AutoCompletionBinding<String> autoCompletionBinding;
-//    Text text = new Text();
-//    text.setFont(Font.font ("Verdana", 20));
-//    text.setFill(Color.RED);
+
     public void click(){
         String text = textField.getText();
         textArea.setText("");
@@ -46,25 +45,45 @@ public class Controller implements Initializable {
         }
         if (dem==0) textArea.setText("Can not find this word.");
     }
-
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
             for(Word w : listWord){
                 listView.getItems().add(w.getWord_target());
             }
-            listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-            textField.setOnKeyReleased(event -> {
-                if (textField.getText() != null) {
-                    listView = null;
-                    for ( Word w : listWord){
-                        if (w.getWord_target().startsWith(textField.getText())==true) {
-                            listView.getItems().add(w.getWord_target());
-                        }
+            textField.setOnKeyReleased(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+                    listView.getItems().clear();
+                    for(Word w : listWord){
+                        if (w.getWord_target().startsWith(textField.getText())==true) listView.getItems().add(w.getWord_target());
                     }
                     listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
                 }
-                event.consume();
             });
-    }
-
+            listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    String selectedItem = (String) listView.getSelectionModel().getSelectedItem();
+                    for (Word w : listWord){
+                        if (selectedItem.equals(w.getWord_target())) {
+                        textArea.setText(w.getWord_explain());
+                        textArea1.setText(Character.toUpperCase(w.getWord_target().charAt(0)) + w.getWord_target().substring(1));
+                        break;
+                }}}
+            });
+            listView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+                    if (keyEvent.getCode() == KeyCode.ENTER) {
+                        String selectedItem = (String) listView.getSelectionModel().getSelectedItem();
+                        for (Word w : listWord){
+                            if (selectedItem.equals(w.getWord_target())) {
+                                textArea.setText(w.getWord_explain());
+                                textArea1.setText(Character.toUpperCase(w.getWord_target().charAt(0)) + w.getWord_target().substring(1));
+                                break;
+                            }}
+                    }
+                }
+            });
+        }
 }
